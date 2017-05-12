51e9bffd7e17834311670b4e8517ac50fb8cad58
Mateusz Kucz <mati.kucz95@gmail.com>
2017-05-11 21:07:30 -0700

    New Test Directories: FINISH UNIT TESTING

---
+++ app/src/androidTest/java/tests/AppPreferenceTest.java
@@ -0,0 +1,9 @@
+package tests;
+
+/**
+ * Created by mkucz on 5/11/2017.
+ */
+
+
+public class AppPreferenceTest {
+}
---
+++ app/src/androidTest/java/tests/PhotoTest.java
@@ -0,0 +1,8 @@
+package tests;
+
+/**
+ * Created by mkucz on 5/11/2017.
+ */
+
+public class PhotoTest {
+}
