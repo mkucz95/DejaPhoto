ee1e6dbc079d684ae2c87c14508f898b759482bb
Mateusz Kucz <mati.kucz95@gmail.com>
2017-06-03 14:08:49 -0700

    JIRA GROUP33-143

--- app/src/main/java/com/example/android/DisplayActivity.java
+++ app/src/main/java/com/example/android/DisplayActivity.java
@@ -1,10 +1,13 @@
 package com.example.android;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -25,65 +28,57 @@ public class DisplayActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         user = (Switch) findViewById(R.id.s_displayUser);
-        if(Global.displayUser) {
+        if (Global.displayUser) {
             user.setChecked(true);
-        }
-        else {
+        } else {
             user.setChecked(false);
         }
 
         friend = (Switch) findViewById(R.id.s_displayFriend);
-        if(Global.displayFriend) {
+        if (Global.displayFriend) {
             friend.setChecked(true);
-        }
-        else {
+        } else {
             friend.setChecked(false);
         }
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         user.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if (isChecked) {
-                        Global.displayUser = true;
-                        Toast.makeText(getApplicationContext(),
-                                "Display User photo", Toast.LENGTH_SHORT).show();
-                        ViewControl.displayOwn(true);
-                    } else {
-                        Global.displayUser = false;
-                        Toast.makeText(getApplicationContext(),
-                                "Will not display currUser photo", Toast.LENGTH_SHORT).show();
-                        ViewControl.displayOwn(false);
-                    }
+                if (isChecked) {
+                    Global.displayUser = true;
+                    Toast.makeText(getApplicationContext(),
+                            "Display User photo", Toast.LENGTH_SHORT).show();
+                    ViewControl.displayOwn(true);
+                } else {
+                    Global.displayUser = false;
+                    Toast.makeText(getApplicationContext(),
+                            "Will not display currUser photo", Toast.LENGTH_SHORT).show();
+                    ViewControl.displayOwn(false);
+                }
             }
         });
 
         friend.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if (isChecked) {
-                        Global.displayFriend = true;
-                        Toast.makeText(getApplicationContext(),
-                                "Display friend photo", Toast.LENGTH_SHORT).show();
-                        ViewControl.displayFriend(true);
-                    } else {
-                        Global.displayFriend = false;
-                        Toast.makeText(getApplicationContext(),
-                                "Will not display friend photo", Toast.LENGTH_SHORT).show();
-                        ViewControl.displayFriend(false);
-                    }
+                if (isChecked) {
+                    Global.displayFriend = true;
+                    Toast.makeText(getApplicationContext(),
+                            "Display friend photo", Toast.LENGTH_SHORT).show();
+                    ViewControl.displayFriend(true);
+                } else {
+                    Global.displayFriend = false;
+                    Toast.makeText(getApplicationContext(),
+                            "Will not display friend photo", Toast.LENGTH_SHORT).show();
+                    ViewControl.displayFriend(false);
+                }
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
