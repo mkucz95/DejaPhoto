51fc9e43df309cf781051ee217cbc7b1f9ba3681
wl36901 <wl36901@gmail.com>
2017-06-03 11:03:58 +0800

    test1

--- app/src/androidTest/java/MS2/test.java
+++
@@ -1,8 +0,0 @@
-package MS2;
-
-/**
- * Created by wl36901 on 2017/6/3.
- */
-
-public class test {
-}
--- app/src/androidTest/java/MS2/testReplaceData.java
+++ app/src/androidTest/java/MS2/testReplaceData.java
@@ -1,35 +1,8 @@
 package MS2;
 
-import android.support.test.rule.ActivityTestRule;
-import android.widget.Button;
-
-import com.example.android.AddFriendsActivity;
-import com.example.dejaphoto.R;
-
-import org.junit.Rule;
-import org.junit.Test;
-
-import static com.example.android.AddFriendsActivity.replaceData;
-import static junit.framework.Assert.assertEquals;
-
 /**
  * Created by wl36901 on 2017/6/3.
  */
 
-public class TestUpdateUI {
-
-    @Rule
-    public ActivityTestRule<AddFriendsActivity> addfActivity = new ActivityTestRule<AddFriendsActivity>(AddFriendsActivity.class);
-
-    @Test
-    public void test1() {
-      //AddFriendsActivity adf = AddFriendsActivity.
-        String input = "hello. world";
-       // replaceData(input);
-
-        assertEquals("hello, world", replaceData(input));
-
-    }
-
-
+public class testReplaceData {
 }
