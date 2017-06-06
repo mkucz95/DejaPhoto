cade55538caddb348091125cd36b65384749bc21
Justin <jklee137@gmail.com>
2017-06-05 18:12:13 -0700

    JIRA33-98 null check on startup, paths in uploadIQ

---
+++ app/src/androidTest/java/MS2/AddPhotoTest.java
@@ -0,0 +1,10 @@
+package MS2;
+
+/**
+ * Created by Justin on 6/5/17.
+ */
+
+public class AddPhotoTest {
+
+
+}
--- app/src/main/java/com/example/android/DatabaseSync.java
+++ app/src/main/java/com/example/android/DatabaseSync.java
@@ -17,49 +17,58 @@ public class DatabaseSync extends TimerTask {
 
     @Override
     public void run() {
-        Log.i(TAG, "uploading images");
 
-        StorageReference storageReference;
+        Log.i(TAG, "USER: " + Global.currUser);
 
-        PhotoStorage photoStorage;
+        if (Global.currUser != null) {
+            Log.i(TAG, "uploading images");
 
-        storageReference = PhotoStorage.getStorageRef(Global.currUser.email);
-        Log.i(TAG, "storageRef: " + storageReference);
+            StorageReference storageReference;
 
-        Log.d("storageReference", "value" + storageReference);
+            PhotoStorage photoStorage;
+            Log.i(TAG, "curemail: " + Global.currUser.email);
 
-        for(Photo photo: Global.uploadImageQueue){
-            photoStorage = new PhotoStorage(photo.getPath(), storageReference);
-            photoStorage.addElement();
-        }
 
-   if(Global.shareSetting) {  //if sharing is on upload images
-    for (Photo photo : Global.uploadImageQueue) {
-        photoStorage = new PhotoStorage(photo.getPath(), storageReference);
-        photoStorage.addElement();
-    }
-}
-        Log.i(TAG, "downloading images");
-        if(PhotoStorage.dirExists("DejaPhotoFriends")){
-            ArrayList<String> friends = Friends.getFriends(Global.currUser.email);
-            for(String friend: friends) {
+            storageReference = PhotoStorage.getStorageRef(Global.currUser.email);
 
-                PhotoStorage.downloadImages( PhotoStorage.getStorageRef(friend)
-                        , "sdcard/DejaPhotoFriends");
+            Log.i(TAG, "storageRef: " + storageReference);
+            Log.d("storageReference", "value" + storageReference);
+
+            for (String photoPath : Global.uploadImageQueue) {
+                photoStorage = new PhotoStorage(photoPath, storageReference);
+                photoStorage.addElement();
+            }
+
+
+            if (Global.shareSetting) {  //if sharing is on upload images
+                for (String photoPath : Global.uploadImageQueue) {
+                    photoStorage = new PhotoStorage(photoPath, storageReference);
+                    photoStorage.addElement();
+                }
+            }
+
+            Log.i(TAG, "downloading images");
+            if (PhotoStorage.dirExists("DejaPhotoFriends")) {
+                ArrayList<String> friends = Friends.getFriends(Global.currUser.email);
+                for (String friend : friends) {
+
+                    PhotoStorage.downloadImages(PhotoStorage.getStorageRef(friend)
+                            , "sdcard/DejaPhotoFriends");
+                }
+            }
+
+            if (Global.displayFriend) { //if we want to see our own
+                Log.i(TAG, "downloading images");
+                if (PhotoStorage.dirExists("DejaPhotoFriends")) {
+                    ArrayList<String> friends = Friends.getFriends(Global.currUser.email);
+                    for (String friend : friends) {
+
+                        PhotoStorage.downloadImages(PhotoStorage.getStorageRef(friend)
+                                , "sdcard/DejaPhotoFriends");
+                    }
+                }
             }
         }
-
-        if(Global.displayFriend) { //if we want to see our own
-          Log.i(TAG, "downloading images");
-          if (PhotoStorage.dirExists("DejaPhotoFriends")) {
-              ArrayList<String> friends = Friends.getFriends(Global.currUser.email);
-              for (String friend : friends) {
-
-                  PhotoStorage.downloadImages(PhotoStorage.getStorageRef(friend)
-                          , "sdcard/DejaPhotoFriends");
-              }
-          }
-      }
     }
 }
 
